cee057af66d29ca2c31212b79a18077d4256c080
juandavid764 <juandaviderazo2401@gmail.com>
2025-06-27 21:53:58 -0500

    public img1

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -36,7 +36,7 @@ const CTA = () => {
           {/* Left side - Image */}
           <div className="flex justify-center">
             <img 
-              src="/public/img1.png" 
+              src="/img1.png" 
               alt="Vicky IA Dashboard Analytics" 
               className="w-full max-w-lg rounded-xl shadow-2xl"
             />
